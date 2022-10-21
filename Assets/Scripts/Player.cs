58b24a62581585b51c50b6830bbbbb63b233748c
Phuc <zololc01@gmail.com>
2022-10-21 17:53:45 +0700

    phuc commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public MovementJoystick joystick;
+    [SerializeField]
     public float playerSpeed;
     private Rigidbody2D rb;
+    Vector3 movementVector;
+    Animate animate;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        movementVector = new Vector3();
+        animate = GetComponent<Animate>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
         if (joystick.joystickVec.y != 0)
         {
             rb.velocity = new Vector2(joystick.joystickVec.x * playerSpeed, joystick.joystickVec.y * playerSpeed);
