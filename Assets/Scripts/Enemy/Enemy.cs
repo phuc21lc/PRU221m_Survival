841e93194d14ade25780cc48a473dd116064e7f6
Phuc <zololc01@gmail.com>
2022-11-01 17:44:45 +0700

    phuc commit

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Transform target;
     GameObject targetGameObj;
     Character targetCharacter;
     [SerializeField] float speed;
@@ -13,28 +14,39 @@ public class Enemy : MonoBehaviour
     Rigidbody2D rb;
 
     [SerializeField]
-    int hp = 999;
+    GameObject healOrbPrefab;
+
+    [SerializeField]
+    int hp = 10;
     [SerializeField]
     int dmg = 1;
     // Start is called before the first frame update
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        targetGameObj = target.gameObject;
+        targetGameObj = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 direction = (target.position - transform.position).normalized;
+        Vector3 direction = (targetGameObj.transform.position - transform.position).normalized;
         rb.velocity = direction * speed;
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.gameObject == targetGameObj)
+        if (collision.gameObject == targetGameObj)
         {
             Attack();
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag.Equals("Bullet"))
+        {
+            takeDamage(FindObjectOfType<Bullet>().dmg);
         }
     }
 
@@ -45,5 +57,20 @@ public class Enemy : MonoBehaviour
             targetCharacter = targetGameObj.GetComponent<Character>();
         }
         targetCharacter.takeDamage(dmg);
+    }
+    private void takeDamage(int damage)
+    {
+        hp -= damage;
+        Debug.Log(hp);
+        if (hp <= 0)
+        {
+            int a = Random.Range(1, 5);
+            if (a == 1)
+            {
+                GameObject arrow = Instantiate(healOrbPrefab, this.transform.position, Quaternion.identity);
+            }
+            //GameObject arrow = Instantiate(healOrbPrefab, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-public void PlayGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene("SampleScene");
     }
