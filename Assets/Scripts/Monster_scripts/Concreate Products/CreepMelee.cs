09859110bb19f66a754fe2b1e5aec72eab4bb7ce
phuc21lc <94586462+phuc21lc@users.noreply.github.com>
2022-11-01 19:58:43 +0700

    Revert "Commit monster without design pattern"

---
+++ Assets/Scripts/Monster_scripts/Concreate Products/CreepMelee.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreepMelee : MonoBehaviour, IMonster
+{
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+}
